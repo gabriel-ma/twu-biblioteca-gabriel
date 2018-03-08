d965635add78c672c05f685198c21b58250c7373
Gabriel Machado <gabrielmachado@Gabriels-MacBook-Pro.local>
2018-03-08 14:46:34 -0300

    added book class

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -5,4 +5,17 @@ public class BibliotecaApp {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
     }
+
+    public String printWelcomeMessage() {
+        return "Welcome to Biblioteca";
+    }
+
+    public String listAvaliableBooks() {
+        return generateLines() + books.listAvaliableBooks();
+    }
+
+    private String generateLines(){
+        return "------------------------------\n";
+    }
+
 }
--- test/com/twu/biblioteca/ExampleTest.java
+++ test/com/twu/biblioteca/ExampleTest.java
@@ -1,13 +1,28 @@
 package com.twu.biblioteca;
 
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class ExampleTest {
+    BibliotecaApp biblioteca;
+    @Before
+    public void setUp() {
+        biblioteca = new BibliotecaApp();
+    }
 
     @Test
-    public void test() {
-        assertEquals(1, 1);
+    public void printWelcomeMessageTest() {
+
+        Assert.assertEquals("Welcome to Biblioteca", biblioteca.printWelcomeMessage());
     }
+
+    @Test
+    public void listAvaliableBooksTest() {
+        assertEquals("1. book 1\n2. book 2\n3. book 3", biblioteca.listAvaliableBooks());
+    }
+
+
 }
